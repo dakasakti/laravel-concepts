07d54f9b4a1f1bcd21afd940dbcf24732e37e298
Mahmuda Karima <dakasakti.id@gmail.com>
2023-08-04 23:36:13 +0700

    materi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+class ServiceOperator
+{
+    public function index()
+    {
+        echo "Class <strong>ServiceOperator</strong> with method index. ";
+        return $this;
+    }
+
+    public function zero()
+    {
+        echo "Object <strong>Zero</strong>";
+        return $this;
+    }
+
+    public function container()
+    {
+        echo "Object <strong>Container</strong>";
+        return $this;
+    }
+
+    public function show(string $name)
+    {
+        echo "<strong>Hide Implement</strong>. ";
+        echo "Call method Static as Non Static. ";
+        echo "Object <strong>Facade</strong> with argument is <strong>$name</strong>";
+        return $this;
+    }
+}
+
+Route::get('/zero', function () {
+    $service = new ServiceOperator();
+    $service->index()->zero();
+});
+
+Route::get('/container', function (ServiceOperator $service) {
+    $service->index()->container();
+});
+
+class Operator extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        // return ServiceOperator::class;
+        return 'serviceoperator';
+    }
+}
+
+Route::get('/facade', function () {
+    Operator::index()->show("dakasakti");
 });
